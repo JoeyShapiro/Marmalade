ccc651088445ee3b6a10828be1590f70222441d8
JoeyShapiro <joeyashapiro@gmail.com>
2022-01-25 13:49:25 -0600

    added ghost, cursor, and bounds

--- Marmalade/Program.cs
+++ Marmalade/Program.cs
@@ -10,50 +10,57 @@ var screen = NCurses.InitScreen();
 
 NCurses.NoDelay(screen, true);
 NCurses.NoEcho();
+NCurses.SetCursor(0);
 
 int kin = -1;
 int i = 0;
 int itemNo = 1;
 const int SIZE = 8;
-Ship ship = new Ship(4);
+//Ship ship = new Ship(4, "test");
 // map
 int[,] map = new int[SIZE,SIZE];
 Ship[] ships = new Ship[32]; // abritrary size
-
-void placeShip(Ship ship, int x, int y, int rot) // smert
+// player
+bool curRot = false;
+int x = 0;
+int y = 0;
+Ship[] ships2Place = // need to add elsewhere, since placeShip() handles in ship, should work
 {
-    if (rot is 0) // up(right)
+    new Ship(2, "Patrol Boat"),
+    new Ship(3, "Submarine"),
+    new Ship(3, "Cruiser"),
+    new Ship(4, "Battleship"),
+    new Ship(5, "Carrier")
+}; // the ships the player has to place // do they have names
+int shipsPlaced = 0;
+
+void placeShip(Ship ship, int x, int y, bool rot) // smert // with ship super smert
+{
+    if (!rot) // up(right)
     {
-        // check if other
-        if (x + ship.size > SIZE)
+        if (x + ship.size > SIZE) // check if in range, only needs one, others are done elseware
             return;
+        // check if other
+        for (int i = 0; i < ship.size; i++)
+            if (map[x + i, y] > 0)
+                return;
+        // add to map
         for (int i = 0; i < ship.size; i++)
             map[x + i, y] = itemNo;
-    }
-    else if (rot is 1)
+    } 
+    else if (rot) // rotated 90 degrees
     {
-        if(x - ship.size < 0)
+        if(y + ship.size > SIZE) // maybe do at handler
             return;
+        // check if other
         for (int i = 0; i < ship.size; i++)
-            map[x - i, y] = itemNo;
-    } // down
-        
-    else if (rot is 2)
-    {
-        if(y + ship.size > SIZE)
-            return;
+            if (map[x, y+i] > 0)
+                return;
         for (int i = 0; i < ship.size; i++)
             map[x, y + i] = itemNo;
-    } // left
-        
-    else if (rot is 3)
-    {
-        if(y - ship.size < 0)
-            return;
-        for (int i = 0; i < ship.size; i++)
-            map[x, y - i] = itemNo;
-    } // right
+    }
     ships[itemNo++] = ship; // store in array with its # as index
+    shipsPlaced++; // do here, so it only does after good, maybe have return true, but this is fine too
         
 }
 
@@ -61,13 +68,49 @@ void drawMap()
 {
     for (int i=0; i<SIZE; i++)
         for (int j=0; j<SIZE; j++)
-            NCurses.MoveAddString(i+1, j+1, ""+map[i,j]);
+            if (map[i, j] is not 0) // smert??, YES, dont show 0, but dont need to print ' ' for 0
+                NCurses.MoveAddString(i+1, j+1, ""+map[i,j]);
+}
+
+void handleInput(int kin)
+{
+    if (kin is (int)'w' && x-1 > -1)
+        x--;
+    else if (kin is (int)'s' && x+1 < SIZE)
+        x++;
+    else if (kin is (int)'a' && y-1 > -1)
+        y--;
+    else if (kin is (int)'d' && y+1 < SIZE)
+        y++;
+    else if (kin is (int)' ')
+    {
+        if (shipsPlaced < 5)
+        {
+            Ship ship2place = ships2Place.ElementAt<Ship>(shipsPlaced); // i remembered how it works :) // should go through each one, and hide the last
+            placeShip(ship2place, x, y, curRot);
+        }
+
+    }
+    else if (kin is (int)'r')
+        curRot = !curRot;
+    else if (kin is 27) // 'esc' || '^['
+        keepGoing = false; // TODO keep global?
+}
+
+void drawGhost(Ship sh) // use local or main when pass
+{
+    if (curRot)
+        for (int i = 0; i < sh.size; i++)
+            NCurses.MoveAddChar(x + 1, y + 1 + i, 'g'); // use "1" to account for map change with view
+    else
+        for (int i = 0; i < sh.size; i++)
+            NCurses.MoveAddChar(x + 1+i, y + 1, 'g');
 }
 
 NCurses.MoveAddString(0, 0, "press any key to continue...");
-placeShip(ship, 0, 0, 0);
-placeShip(ship, 0, 0, 1);
-placeShip(ship, 0, 0, 2);
+//placeShip(ship, 0, 0, false);
+//placeShip(ship, 0, 0, true); // shouldnt be placed // does this add to itemNO // no because it returns early
+//placeShip(ship, 0, 4, true);
 
 while(keepGoing) {
     kin = NCurses.GetChar(); // its async
@@ -76,16 +119,20 @@ while(keepGoing) {
         continue;
 
     NCurses.Clear();
+    handleInput(kin);
     // draw layout
     drawMap();
+    if (shipsPlaced < 5)
+        drawGhost(ships2Place.ElementAt<Ship>(shipsPlaced)); // maybe just make nextShip and use throughout, should work
     NCurses.Box(screen, '*', '*');
-    NCurses.MoveAddChar(17,10, (char)kin);
-    string str = "times: " + i++ + "char: (char)" + kin;
+    NCurses.MoveAddString(SIZE+1, 1, "******");
+    NCurses.MoveAddString(1, SIZE+1, "*");
+    string str = "char: (char)" + kin + " char: " + (char)kin + " times: " + i++;
     NCurses.MoveAddString(18, 0, str);
+    NCurses.MoveAddChar(x+1, y+1, 'x'); // player, must be after map
     NCurses.Refresh();
 
-    if (kin is 27) // 'esc' || '^['
-        keepGoing = false;
+    
 };
 
 NCurses.EndWin();
--- Marmalade/Ship.cs
+++ Marmalade/Ship.cs
@@ -5,10 +5,12 @@ namespace Marmalade
 	public class Ship
 	{
 		public int size;
+		public string name;
 
-		public Ship(int s)
+		public Ship(int s, string n)
 		{
 			size = s;
+			name = n;
 		}
 	}
 }
