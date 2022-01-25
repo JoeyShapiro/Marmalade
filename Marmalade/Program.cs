4e69e655b5ec05ab5604f092087b80a2b481a602
JoeyShapiro <joeyashapiro@gmail.com>
2022-01-25 10:38:35 -0600

    started

--- Marmalade/Program.cs
+++ Marmalade/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Mindmagma.Curses;
+using Marmalade;
 
 bool keepGoing = true;
 
@@ -6,22 +7,35 @@ bool keepGoing = true;
 Console.WriteLine("Hello, World!");
 
 var screen = NCurses.InitScreen();
+var screenPlayer = NCurses.NewWindow(20, 30, 0, 0);
+var screenEnemy = NCurses.NewWindow(21, 31, 21, 20);
 
 NCurses.NoDelay(screen, true);
 NCurses.NoEcho();
 
-int kin = 0;
+int kin = -1;
 int i = 0;
+Ship ship = new Ship(4);
+// map
+
+NCurses.MoveAddString(0, 0, "press any key to continue...");
 
 while(keepGoing) {
-    kin = NCurses.GetChar();
+    kin = NCurses.GetChar(); // its async
+
+    if (kin is -1) // deal with async??
+        continue;
+
     NCurses.Clear();
+    // draw layout
+    NCurses.Box(screenPlayer, '*', '*');
+    NCurses.Box(screenEnemy, '*', '*');
     NCurses.MoveAddChar(5,5, (char)kin);
-    string str = "times: " + i++;
+    string str = "times: " + i++ + "char: (char)" + kin;
     NCurses.MoveAddString(6, 6, str);
     NCurses.Refresh();
 
-    if (kin is (int)'q')
+    if (kin is 27) // 'esc' || '^['
         keepGoing = false;
 };
 
---
+++ Marmalade/Ship.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Marmalade
+{
+	public class Ship
+	{
+		int size;
+
+		public Ship(int s)
+		{
+			size = s;
+		}
+	}
+}
+
